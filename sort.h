290823d9e7337b6d8cc6e8665e095b01df90f9d3
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-02-17 08:38:21 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -15,9 +15,9 @@
 
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -27,4 +27,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif
+#endiif
