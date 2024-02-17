b48afc88dd0b205d364bb9c4c2ac774122d8b5d9
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-02-17 09:16:29 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
+		swapped = false;
 
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -28,7 +28,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
-				swapped = 1;
+				swapped = true;
 				print_array(array, size);
 			}
 		}
