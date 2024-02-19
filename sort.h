1507e197535513e7a1b6051f95a1ea1436491a9c
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-02-19 06:50:30 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -33,5 +33,7 @@ void swap(int *x, int *y);
 
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+bool left_bubble_sort(listint_t **list, listint_t **current);
+bool right_bubble_sort(listint_t **list, listint_t **current);
 
 #endif
