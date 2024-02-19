d685e77eda64500a8b1a86ec54fa20992f75409c
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-02-19 06:44:53 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -32,5 +32,6 @@ int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *x, int *y);
 
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
