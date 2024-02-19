7214a1051412e9d3c675b9a344d8e0ea0dd1553f
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-02-19 05:55:04 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -27,4 +27,10 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void quicksort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void swap(int *x, int *y);
+
+void shell_sort(int *array, size_t size);
+
 #endif
