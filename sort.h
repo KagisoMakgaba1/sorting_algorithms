963c4ffbd973774e30d7e33a8d6925ce6b63d73c
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-02-20 02:00:25 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -25,12 +25,11 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t siize);
 
-void quicksort_recursive(int *array, int low, int high, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
-void swap(int *x, int *y);
+void quick_sort(int *array, size_t size);
+void lomuto_sort(int *array, size_t size, int left, int right);
+int lomuto_partition(int *array, size_t size, int left, int right);void swap_ints(int *a, int *b);
 
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
