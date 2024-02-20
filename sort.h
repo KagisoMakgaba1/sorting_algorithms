5defae2b30666bd87f42db94b611d4c104fbd0e8
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-02-20 02:02:05 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -29,7 +29,8 @@ void selection_sort(int *array, size_t siize);
 
 void quick_sort(int *array, size_t size);
 void lomuto_sort(int *array, size_t size, int left, int right);
-int lomuto_partition(int *array, size_t size, int left, int right);void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void swap_ints(int *a, int *b);
 
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
